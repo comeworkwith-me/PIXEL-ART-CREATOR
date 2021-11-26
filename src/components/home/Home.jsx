436fe655a5d899afabacd730cd18b02f3b4cf4de
comeworkwith-me <78459369+comeworkwith-me@users.noreply.github.com>
2021-11-26 14:44:42 -0500

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,12 +8,16 @@ export default function Home() {
     id="home-component"
     >
       <h1 className="h1">CREATE PIXEL ART!</h1>
-      <p className="paragraph">From self portraits to flowers, <br/> create original pixel art at the click of your mouse. </p>
+      <img src="https://gateway.pinata.cloud/ipfs/QmeRRZ7Fn112GH7P2GM1JR169VThYb4fxuuUWWpwdkN441" alt="Girl in a jacket" width="500" height="300"/>
+
+      <p className="paragraph">From self portraits to flowers, create original <br/> works of pixel art with the click of your mouse. </p>
+
       <h2>FEATURES</h2>
-      <p>Color Select</p>
-      <p>Export as PNG</p>
-      <p>Reset Canvas</p>
-      <p>Custom Canvas Size</p>
+
+      <p className="p-list">Color Select</p>
+      <p className="p-list">Reset Canvas</p>
+      <p className="p-list">Export as PNG</p>
+      <p className="p-list">Custom Canvas Size</p>
     
     </div>
 

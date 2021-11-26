98357d6139263effe238e3830e173f52b182959e
comeworkwith-me <78459369+comeworkwith-me@users.noreply.github.com>
2021-11-26 14:43:05 -0500

    Add files via upload

---
+++ src/components/blog/Blog.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Blog.css"
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>MINTBEAN HACKATHON</h1>
+      <p className="blog">
+      Participating in my first Mintbean hackathon, I immersed myself in the experience. From the kickoff event to the closing workshop, I engaged with the encouraging Discord community daily. Full of varied projects, including polaroid doodling & coloring books, advanced apps also combined backend technologies to record user drawings as a gallery display.<br/><br/>
+
+      Challenged to create an interactive visual app during a one-week sprint, I developed and designed this pixel art creator. Users create pixel art using a color pallet to paint pixels, resulting in original works of art.<br/><br/>
+
+      To develop this project, I used HTML, CSS, and React. I started by setting up my Replit environment with the needed packages and organized my app structure using components. Using Replit allowed for rapid and constant deployment, making it easy to debug as I create.<br/><br/>
+
+      Once my base environment was set up, I :
+
+      <ul>1. Completed the pixel editor and added the color wheel.</ul>
+      <ul>2. Designed the UI/UX, including coding the pixel art using CSS.</ul>
+      <ul>3. Added the additional features: popup instructions and navigation.</ul>
+
+      Challenges:
+
+      The biggest challenge was problem-solving a landing page. React is widely used to create single-page apps, not multipage websites. I did not want an external landing page linking to the app, so I researched concepts to provide an alternative. I learned about React Router DOM with my research and found the base use easy to understand and worth implementation. This allowed me to render components as single web pages with distinct browser endings.<br/><br/>
+
+      Mintbean is an excellent resource for junior developers. Having a space that combats the biases and challenges junior developers face while trying to land their first career placement is a needed support system. Their hiring hackathon was a really great experience, and I look forward to challenging myself with the next one!<br/><br/>
+
+      Github Repo: https://github.com/comeworkwith-me/PIXEL-ART-CREATOR
+
+      Twitter: https://twitter.com/scarlett_spade
+      
+      </p>
+    </div>
+  
+  )
+}
